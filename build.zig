dddab7c78022b2bdd9c878169f2cf72833a22d70
braheezy <11700503+braheezy@users.noreply.github.com>
2025-05-13 21:44:02 -0700

    Use zibra.zig

--- build.zig
+++ build.zig
@@ -6,7 +6,7 @@ pub fn build(b: *std.Build) !void {
     const optimize = b.standardOptimizeOption(.{});
 
     const source_module = b.createModule(.{
-        .root_source_file = b.path("src/main.zig"),
+        .root_source_file = b.path("src/zibra.zig"),
         .target = target,
         .optimize = optimize,
     });
